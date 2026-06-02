c5699ee9803581796f0bfc46f5477da9d9a150e8
wjapraaa <wjapraaa@gmail.com>
2026-06-02 20:57:17 +0700

    Create paralel.sh

---
+++ paralel.sh
@@ -0,0 +1,13 @@
+i=1
+while read token; do
+  tid="${token%%:*}"
+  tsec="${token##*:}"
+  screen -dmS "nasa-$(printf '%02d' $i)" bash -c "
+    export MODAL_TOKEN_ID='$tid'
+    export MODAL_TOKEN_SECRET='$tsec'
+    modal run /home/ubuntu/ml_train.py
+  "
+  echo "🚀 nasa-$i launched"
+  ((i++))
+  sleep 2
+done < tokens.txt
